0833adc6b9bab826faf1aed01199494678230364
MaxQiu0108 <maxqqqq@hotmail.com>
2022-04-27 15:42:26 +0800

    Test.py
    TEST

---
+++ Test.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:        Test
+# Purpose: Test
+#
+# Author:      Max Qiu
+#
+# Created:     07/03/2022
+# Copyright:   (c)MAxqiu2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+print("this is a testing message")
