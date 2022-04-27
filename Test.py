82026b443429ba826eb3d5938cad0543a7c7566a
MaxQiu0108 <maxqqqq@hotmail.com>
2022-04-27 15:43:53 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,5 @@
-#-------------------------------------------------------------------------------
-# Name:        Test
-# Purpose: Test
-#
-# Author:      Max Qiu
-#
-# Created:     07/03/2022
-# Copyright:   (c)MAxqiu2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 print("this is a testing message")
+a = 0
+While True:
+  a += 1
+  print(a)
